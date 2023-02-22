be5b8410f7f6eeabcc6180907adf369208006b56
Ebtehal612 <86927828+Ebtehal612@users.noreply.github.com>
2023-02-23 01:02:43 +0200

    Update e2.c

--- C/lec 3/Assignment 2/e2.c
+++ C/lec 3/Assignment 2/e2.c
@@ -6,7 +6,7 @@ int main()
     fflush(stdin);
     fflush(stdout);
     scanf("%c", &n);
-    if (n == 'a' || n == 'e' || n == || n == 'i' || n == 'o' || n == 'A' || n == 'I' || n == || n == 'E' || n == 'O')
+    if (n == 'a' || n == 'e' || n == 'i' || n == 'o' || n == 'A' || n == 'I' || n == 'E' || n == 'O')
     {
         printf("%c is a vowel", n);
     }
